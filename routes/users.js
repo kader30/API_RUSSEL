2aa1273f453b05b9e0f8b3d7309755c3d49c9716
Abdelkader Bensadi <abdelkaderbensadi@gmail.com>
2026-01-23 23:35:33 +0100

    ajouter des views

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const auth = require("../middlewares/auth.middleware");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+router.get("/", auth, async (req, res) => {
+  res.render("users", { users: await User.find() });
+});
+
+router.post("/", auth, async (req, res) => {
+  req.body.password = await bcrypt.hash(req.body.password, 10);
+  await User.create(req.body);
+  res.redirect("/users");
+});
+
+router.post("/delete/:email", auth, async (req, res) => {
+  await User.deleteOne({ email: req.params.email });
+  res.redirect("/users");
 });
 
 module.exports = router;
